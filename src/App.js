1e2c11f26eb684edf0bc0ef034b125d0a508d7e3
임학수 <hsim@miridih.com>
2023-03-07 21:23:33 +0900

    feat: recoil의 atom, selector 사용하여 todo list getting started 학습

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { TodoList } from "./TodoList";
+
+import "./App.css";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <TodoList />
+    </RecoilRoot>
+  );
+}
+
+export default App;
