b5b99eb6d8d2d2c55fe220b3512ad2c342a3331c
임학수 <hsim@miridih.com>
2023-03-12 20:51:33 +0900

    fix: atom 분리

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,56 +1,11 @@
-import {
-  useSetRecoilState,
-  useRecoilValue,
-  useRecoilState,
-  atom,
-  selector,
-} from "recoil";
+import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
 import { useState } from "react";
-
-const todoListState = atom({
-  key: "todoListState",
-  default: [],
-});
-
-const todoListFilterState = atom({
-  key: "todoListFilterState",
-  default: "Show All",
-});
-
-const filteredTodoListState = selector({
-  key: "filteredTodoListState",
-  get: ({ get }) => {
-    const filter = get(todoListFilterState);
-    const list = get(todoListState);
-
-    switch (filter) {
-      case "Show Completed":
-        return list.filter((item) => item.isComplete);
-      case "Show Uncompleted":
-        return list.filter((item) => !item.isComplete);
-      default:
-        return list;
-    }
-  },
-});
-
-const todoListStatsState = selector({
-  key: "todoListStatsState",
-  get: ({ get }) => {
-    const todoList = get(todoListState);
-    const totalNum = todoList.length;
-    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
-    const totalUncompletedNum = totalNum - totalCompletedNum;
-    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
-
-    return {
-      totalNum,
-      totalCompletedNum,
-      totalUncompletedNum,
-      percentCompleted,
-    };
-  },
-});
+import {
+  filteredTodoListState,
+  todoListFilterState,
+  todoListState,
+  todoListStatsState,
+} from "./atom/todoList";
 
 function TodoListStats() {
   const { totalNum, totalCompletedNum, totalUncompletedNum, percentCompleted } =
